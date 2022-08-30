65a1bf3766b44f40a6bc8a1bdbfb474498d05d39
Chris Bay <chris@chrisbay.net>
2022-08-30 11:17:07 -0500

    Code from first part of ch13 lesson

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
 
 @Controller
 //@RequestMapping(value="basePath")
@@ -31,27 +35,37 @@ public class HelloController {
     // respond to GET requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
     @ResponseBody
-    public String helloPathParams(@PathVariable String name) {
+    public String helloPathParams(String name) {
         return "<h1>Hello, " + name + "!</h1>";
     }
 
     // respond to GET requests at /sayHello, and display a basic form
     @GetMapping("sayHello")
-    @ResponseBody
-    public String helloForm() {
-        return "<form action='/sayHello/submit' method='POST'>" +
-                "<input name='name' type='text'>" +
-                "<button>Greet Me!</button>" +
-                "</form>";
+    public String helloForm(Model model) {
+        model.addAttribute("title", "Custom Greeting Form");
+        return "form";
     }
 
-    // respond to GET requests at /sayHello/submit?name=LaunchCode
-    // *and* to POST requests at /sayHello/submit w/ name=LaunchCode
-    @RequestMapping(value="sayHello/submit", method={RequestMethod.GET, RequestMethod.POST})
-    // can handle only posts w/ @PostMapping("sayHello/submit")
-    @ResponseBody
-    public String processHelloForm(@RequestParam String name) {
-        return "<h1>Hello, " + name + "!</h1>";
+    // respond to POST requests at /sayHello/submit
+    @PostMapping("sayHello/submit")
+    public String processHelloForm(@RequestParam String name, Model model) {
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        model.addAttribute("title", "Your Custom Greeting");
+        return "hello";
+    }
+
+    @GetMapping("list")
+    public String displayList(Model model) {
+
+        ArrayList<String> cheeses = new ArrayList<>();
+//        cheeses.add("white cheddar");
+//        cheeses.add("provolone");
+//        cheeses.add("gouda");
+//        cheeses.add("gorganzola");
+
+        model.addAttribute("cheeses", cheeses);
+
+        return "list";
     }
 
 }
